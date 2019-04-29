4392f047aeb0097b669df58a7a9c018da95df631
timmybobguy <41880797+timmybobguy@users.noreply.github.com>
2019-04-30 10:55:13 +1200

    Starting it #1

--- src/Game.js
+++ src/Game.js
@@ -1,1 +1,18 @@
+var gameApp = new Vue({
+  el: '#main-gameApp',
+  data: {
+    one: '',
+    two: '',
+    three: ''
+  },
+  methods: {
+    generateRandomNumber: function(lowerLimit, upperLimit) {
+      return Math.floor(Math.random() * (+upperLimit - +lowerLimit)) + +lowerLimit;
+    },
+    changeNumber: function() {
+      console.log(this.generateRandomNumber(0,2));
+      //generateRandomNumber(1,10)
+    },
 
+  }
+})
