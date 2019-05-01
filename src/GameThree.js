4e89393cd36fa87ecf22131cead82adbcbcb18e8
timmybobguy <41880797+timmybobguy@users.noreply.github.com>
2019-05-02 09:52:55 +1200

    New

--- src/GameThree.js
+++ src/GameThree.js
@@ -1,43 +1,73 @@
 var gameThree = new Vue({
   el: '#game-Three',
   data: {
-    randomNumber: 0,
+	initalGuess: 0,
+	maximumNumber: 99,
+	minimumNumber: 0,
     numberOfGuesses: 0,
     gameShown: false,
     instructionShown: true,
-    lastGuess: '',
-    lastGuessShown: false,
     alertStart: true,
     alertRight: false,
-    alertCold: false,
-    alertCool: false,
-    alertWarm: false,
-    alertHot: false,
-    alertError: false,
+	alertGuess: false,
+	alertError: false,
     finishGame: true,
   },
   methods: {
-    startgameThree: function() {
+    startGameThree: function() {
+	  this.initialGuess = gameApp.generateRandomNumber(25,75);
       this.numberOfGuesses = 0
       this.instructionShown = false;
       this.gameShown = true;
+	  this.maximumNumber = 99;
 
     },
-    dismissAlerts: function() {
+	dismissAlerts: function() {
       this.instructionShown = false;
       this.alertStart = false;
+      this.alertGuess = false;
       this.alertRight = false;
-      this.alertCold = false;
-      this.alertCool = false;
-      this.alertWarm = false;
-      this.alertHot = false;
       this.alertError = false;
     },
-    quitGame: function() {
+	guessRight: function() {
+	  this.dismissAlerts();
+	  this.alertRight = true;
+	  this.finishGame = false;
+	},
+	guessLower: function() {
+	  this.dismissAlerts();
+	  if (this.initialGuess > this.maximumNumber && this.maximumNumber != 0) {
+		this.alertError = true;
+	  } else {
+		
+		this.maximumNumber = this.initialGuess;
+		this.initialGuess = Math.round(this.initialGuess/2);
+		
+		this.alertGuess = true;
+	  }
+	  this.numberOfGuesses += 1;
+	},
+	guessHigher: function() {
+	  this.dismissAlerts();
+	  if (this.initialGuess > this.maximumNumber && this.minimumNumber != 0) {
+		this.alertError = true;
+	  } else {
 
+	  	this.minimumNumber = this.initialGuess;
+		this.initialGuess = Math.round(this.initialGuess + ((this.maximumNumber - this.minimumNumber) / 2));
+
+		this.alertGuess = true;
+	  }
+	  this.numberOfGuesses += 1;
+	},
+	quitGame: function() {
+	  this.dismissAlerts();
+	  this.gameShown = false;
+	  this.alertStart = true;
+      this.finishGame = true;
       this.instructionShown = true;
       gameApp.playerScore += this.numberOfGuesses;
-    }
+	}
   }
 
 })
