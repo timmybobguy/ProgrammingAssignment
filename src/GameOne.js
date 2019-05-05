03fe64b35a94208b88332422804a239c58f40f6c
timmybobguy <41880797+timmybobguy@users.noreply.github.com>
2019-05-06 01:13:41 +1200

    Fix

--- src/GameOne.js
+++ src/GameOne.js
@@ -12,53 +12,52 @@ var gameOne = new Vue({
     alertLower: false,
     alertRight: false,
     alertError: false,
-    finishGame: true,
+    finishGame: true
   },
   methods: {
-    startGameOne: function() {
-      this.randomNumber = gameApp.generateRandomNumber(0,100);
+    startGameOne: function () {
+      this.randomNumber = gameApp.generateRandomNumber(0, 100)
       this.numberOfGuesses = 0
-      console.log(this.randomNumber);
-      this.instructionShown = false;
-      this.gameShown = true;
-      this.lastGuessShown = false;
-
+      console.log(this.randomNumber)
+      this.instructionShown = false
+      this.gameShown = true
+      this.lastGuessShown = false
     },
-    dismissAlerts: function() {
-      this.instructionShown = false;
-      this.alertStart = false;
-      this.alertHigher = false;
-      this.alertLower = false;
-      this.alertRight = false;
-      this.alertError = false;
+    dismissAlerts: function () {
+      this.instructionShown = false
+      this.alertStart = false
+      this.alertHigher = false
+      this.alertLower = false
+      this.alertRight = false
+      this.alertError = false
     },
-    guessNumber: function() {
-      this.lastGuessShown = true;
-      this.dismissAlerts();
-      var guess = Number(gameApp.getUserInput('gameOneInput'));
-      if (this.randomNumber == guess) {
-        this.alertRight = true;
-        document.getElementById('gameOneInput').readOnly = true;
-        this.finishGame = false;
+    guessNumber: function () {
+      this.lastGuessShown = true
+      this.dismissAlerts()
+      var guess = Number(gameApp.getUserInput('gameOneInput'))
+      if (this.randomNumber === guess) {
+        this.alertRight = true
+        document.getElementById('gameOneInput').readOnly = true
+        this.finishGame = false
       } else if (guess > 99 || guess < 0) {
-        this.alertError = true;
+        this.alertError = true
       } else if (guess < this.randomNumber) {
-        this.alertHigher = true;
+        this.alertHigher = true
       } else if (guess > this.randomNumber) {
-        this.alertLower = true;
+        this.alertLower = true
       }
-      this.numberOfGuesses += 1;
+      this.numberOfGuesses += 1
       this.lastGuess = guess
-      document.getElementById('gameOneInput').value = "";
+      document.getElementById('gameOneInput').value = ''
     },
-    quitGame: function() {
-      document.getElementById('gameOneInput').readOnly = false;
-      this.gameShown = false;
-      this.dismissAlerts();
-      this.alertStart = true;
-      this.finishGame = true;
-      this.instructionShown = true;
-      gameApp.playerScore += this.numberOfGuesses;
+    quitGame: function () {
+      document.getElementById('gameOneInput').readOnly = false
+      this.gameShown = false
+      this.dismissAlerts()
+      this.alertStart = true
+      this.finishGame = true
+      this.instructionShown = true
+      gameApp.playerScore += this.numberOfGuesses
     }
   }
 
